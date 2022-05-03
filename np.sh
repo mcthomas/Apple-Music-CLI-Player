91403d4995dac857367ba411bdcf4322819fbd48
mcthomas <matt.thomas2@icloud.com>
2022-05-02 22:08:52 -0500

    cleanup np.sh

--- np.sh
+++ np.sh
@@ -7,44 +7,40 @@ shuffle=$(osascript -e 'tell application "Music" to get shuffle enabled')
 duration=$(osascript -e 'tell application "Music" to get {player position} & {duration} of current track')
 arr=(`echo ${duration}`)
 curr=$(cut -d . -f 1 <<< ${arr[-2]})
-diff=$(( end - curr ))
 currMin=$(echo $(( curr / 60 )))
 currSec=$(echo $(( curr % 60 )))
-if [ ${#currMin} = 1 ]
-then
+if [ ${#currMin} = 1 ]; then
 	currMin="0$currMin"
 fi
-if [ ${#currSec} = 1 ]
-then
+if [ ${#currSec} = 1 ]; then
 	currSec="0$currSec"
 fi
 if (( curr < 2 || init == 1 )); then
-init=0
-name=$(osascript -e 'tell application "Music" to get name of current track')
-name=${name:0:50}
-artist=$(osascript -e 'tell application "Music" to get artist of current track')
-artist=${artist:0:50}
-record=$(osascript -e 'tell application "Music" to get album of current track')
-record=${record:0:50}
-end=$(cut -d . -f 1 <<< ${arr[-1]})
-endMin=$(echo $(( end / 60 )))
-endSec=$(echo $(( end % 60 )))
-#bar=$(spark 0 $diff ${arr[-1]})
-if [ ${#endMin} = 1 ]
-then
-	endMin="0$endMin"
-fi
-if [ ${#endSec} = 1 ]
-then
-	endSec="0$endSec"
-fi
-rm ~/Library/Scripts/tmp*
-osascript ~/Library/Scripts/album-art.applescript
-if [ -f ~/Library/Scripts/tmp.png ]; then
-	art=$(clear; viu ~/Library/Scripts/tmp.png -w 39 -h 13)
-else 
-	art=$(clear; viu ~/Library/Scripts/tmp.jpg -w 39 -h 13)
-fi
+	init=0
+	name=$(osascript -e 'tell application "Music" to get name of current track')
+	name=${name:0:50}
+	artist=$(osascript -e 'tell application "Music" to get artist of current track')
+	artist=${artist:0:50}
+	record=$(osascript -e 'tell application "Music" to get album of current track')
+	record=${record:0:50}
+	end=$(cut -d . -f 1 <<< ${arr[-1]})
+	endMin=$(echo $(( end / 60 )))
+	endSec=$(echo $(( end % 60 )))
+	if [ ${#endMin} = 1 ]
+	then
+		endMin="0$endMin"
+	fi
+	if [ ${#endSec} = 1 ]
+	then
+		endSec="0$endSec"
+	fi
+	rm ~/Library/Scripts/tmp*
+	osascript ~/Library/Scripts/album-art.applescript
+	if [ -f ~/Library/Scripts/tmp.png ]; then
+		art=$(clear; viu ~/Library/Scripts/tmp.png -w 39 -h 13)
+	else 
+		art=$(clear; viu ~/Library/Scripts/tmp.jpg -w 39 -h 13)
+	fi
 fi
 vol=$(echo $(awk -F ':|,' '{print $2}' <<< $vol))
 if [ $vol = 0 ]; then
