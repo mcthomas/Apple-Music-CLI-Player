f6e4976f3e1bfdda3e9dbc1e8ad7b3641d48a6bf
mcthomas <matt.thomas2@icloud.com>
2022-10-06 17:14:51 -0500

    Add library flag to listen.sh, add shuffle key to np.sh

--- src/list.sh
+++ src/list.sh
@@ -10,53 +10,53 @@ usage="Usage: list.sh [-grouping] [-name]
   -g              List all genres.
   -g PATTERN      List all songs in the genre PATTERN."
 if [ "$#" -eq 0 ]; then
-	echo -e $usage
+	printf '%s\n' "$usage";
 else
-if [ $1 = "-p" ]
-then
-	if [ "$#" -eq 1 ]; then
-		shift
-		osascript -e 'tell application "Music" to get name of playlists' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	else
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get name of every track of playlist (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	fi
-elif [ $1 = "-s" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get name of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3  
+	if [ $1 = "-p" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			shift
+			osascript -e 'tell application "Music" to get name of playlists' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		else
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get name of every track of playlist (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		fi
+	elif [ $1 = "-s" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get name of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		else
+			echo $usage
+		fi
+	elif [ $1 = "-r" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get album of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		else
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose album is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		fi
+	elif [ $1 = "-a" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get artist of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		else
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose artist is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		fi
+	elif [ $1 = "-g" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get genre of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		else
+			shift
+			osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose genre is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
+		fi
 	else
 		echo $usage
 	fi
-elif [ $1 = "-r" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get album of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3  
-	else
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose album is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	fi
-elif [ $1 = "-a" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get artist of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	else
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose artist is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	fi
-elif [ $1 = "-g" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get genre of every track' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	else
-		shift
-		osascript -e 'on run args' -e 'tell application "Music" to get name of every track whose genre is (item 1 of args)' -e 'end' "$*" | tr "," "\n" | sort | awk '!seen[$0]++' | pr -T -a -3
-	fi
-else
-	echo $usage
-fi
 fi
--- src/listen.sh
+++ src/listen.sh
@@ -9,60 +9,64 @@ usage="Usage: listen.sh [-grouping] [-name]
   -p              Fzf for a playlist and begin playback.
   -p PATTERN      Play from the playlist PATTERN.
   -g              Fzf for a genre and begin playback.
-  -g PATTERN      Play from the genre PATTERN."
+  -g PATTERN      Play from the genre PATTERN.
+  -l              Play from your entire library."
 if [ "$#" -eq 0 ]; then
-	echo $usage
+	printf '%s\n' "$usage"
 else
-if [ $1 = "-p" ]
-then
-	if [ "$#" -eq 1 ]; then
-		playlist=$(osascript -e 'tell application "Music" to get name of playlists' | tr "," "\n" | fzf)
-		set -- ${playlist:1}
-	else
-		shift
-	fi
+	if [ $1 = "-p" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			playlist=$(osascript -e 'tell application "Music" to get name of playlists' | tr "," "\n" | fzf)
+			set -- ${playlist:1}
+		else
+			shift
+		fi
+		osascript -e 'on run argv
+			tell application "Music" to play playlist (item 1 of argv)
+		end' "$*"
+	elif [ $1 = "-s" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			song=$(osascript -e 'tell application "Music" to get name of every track' | tr "," "\n" | fzf)
+			set -- ${song:1}
+		else
+			shift
+		fi
 	osascript -e 'on run argv
-		tell application "Music" to play playlist (item 1 of argv)
+		tell application "Music" to play track (item 1 of argv)
 	end' "$*"
-elif [ $1 = "-s" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		song=$(osascript -e 'tell application "Music" to get name of every track' | tr "," "\n" | fzf)
-		set -- ${song:1}
+	elif [ $1 = "-r" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			record=$(osascript -e 'tell application "Music" to get album of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
+			set -- ${record:1}
+		else
+			shift
+		fi
+		osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose album is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
+	elif [ $1 = "-a" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			artist=$(osascript -e 'tell application "Music" to get artist of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
+			set -- ${artist:1}
+		else
+			shift
+		fi
+		osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose artist is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
+	elif [ $1 = "-g" ]
+	then
+		if [ "$#" -eq 1 ]; then
+			genre=$(osascript -e 'tell application "Music" to get genre of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
+			set -- ${genre:1}
+		else
+			shift
+		fi
+		osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose genre is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
+	elif [ $1 = "-l" ]
+	then
+		osascript -e 'tell application "Music"' -e 'play playlist "Library"' -e 'end tell' 
 	else
-		shift
+		echo $usage
 	fi
-osascript -e 'on run argv
-	tell application "Music" to play track (item 1 of argv)
-end' "$*"
-elif [ $1 = "-r" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		record=$(osascript -e 'tell application "Music" to get album of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
-		set -- ${record:1}
-	else
-		shift
-	fi
-	osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose album is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
-elif [ $1 = "-a" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		artist=$(osascript -e 'tell application "Music" to get artist of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
-		set -- ${artist:1}
-	else
-		shift
-	fi
-	osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose artist is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
-elif [ $1 = "-g" ] 
-then
-	if [ "$#" -eq 1 ]; then
-		genre=$(osascript -e 'tell application "Music" to get genre of every track' | tr "," "\n" | sort | awk '!seen[$0]++' | fzf)
-		set -- ${genre:1}
-	else
-		shift
-	fi
-	osascript -e 'on run argv' -e 'tell application "Music"' -e 'if (exists playlist "temp_playlist") then' -e 'delete playlist "temp_playlist"' -e 'end if' -e 'set name of (make new playlist) to "temp_playlist"' -e 'set theseTracks to every track of playlist "Library" whose genre is (item 1 of argv)' -e 'repeat with thisTrack in theseTracks' -e 'duplicate thisTrack to playlist "temp_playlist"' -e 'end repeat' -e 'play playlist "temp_playlist"' -e 'end tell' -e 'end' "$*"
-else
-	echo $usage
-fi
 fi
